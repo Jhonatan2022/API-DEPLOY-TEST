ef9c0913c21172c0e6ddfc88d4e0af3260b90285
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2024-01-01 01:10:39 -0500

    APAI

---
+++ app.js
@@ -0,0 +1,110 @@
+const express = require('express')
+const crypto = require('node:crypto')
+const cors = require('cors')
+const moviesJSON = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./Schemas/movies')
+
+const app = express()
+app.disable('x-powered-by')
+app.use(express.json())
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        'http://localhost:3000',
+        'http://localhost:1234',
+        'http://localhost:4321',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500'
+      ]
+
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true)
+      }
+
+      if (!origin) {
+        return callback(null, true)
+      }
+
+      return callback(new Error('Not allowed by CORS'))
+    }
+  })
+)
+
+// Metodos normales: GET/HEAD/POST
+// Metodos complejos: PUT/DELETE/PATCH
+
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = moviesJSON.filter((movie) =>
+      movie.genre.some(
+        (g) => g.toLocaleLowerCase() === genre.toLocaleLowerCase()
+      )
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(moviesJSON)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = moviesJSON.find((movie) => movie.id === id)
+
+  if (movie) return res.json(movie)
+  res.status(404).json({ error: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ errors: JSON.parse(result.error.message) })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  moviesJSON.push(newMovie)
+  res.status(201).json(newMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = moviesJSON.findIndex((movie) => movie.id === id)
+  if (movieIndex === -1) {
+    return res.status(404).json({ error: 'Movie not found' })
+  }
+
+  moviesJSON.splice(movieIndex, 1)
+  return res.status(204).end()
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const result = validatePartialMovie(req.body)
+  if (!result.success) {
+    return res.status(400).json({ errors: JSON.parse(result.error.message) })
+  }
+
+  const movieIndex = moviesJSON.findIndex((movie) => movie.id === id)
+  if (movieIndex === -1) {
+    return res.status(404).json({ error: 'Movie not found' })
+  }
+
+  const updatedMovie = {
+    ...moviesJSON[movieIndex],
+    ...result.data
+  }
+
+  moviesJSON[movieIndex] = updatedMovie
+  return res.json(updatedMovie)
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`Listening on port http://localhost:${PORT}`)
+})
